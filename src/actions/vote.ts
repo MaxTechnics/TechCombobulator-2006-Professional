df1f7760273824a0b2701dc33cfa86c3b2433ec1
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2025-06-09 22:51:45 +0200

    Add rundown items to vote results

--- src/actions/vote.ts
+++ src/actions/vote.ts
@@ -4,7 +4,8 @@ export type VoteChoice = {
     name: string;
     icon: keyof typeof lucideicon;
     id: string;
-    action?: string; // action to execute i guess. 
+    action?: string; // action to execute i guess.
+    rundownitems?: string[]; // rundown item id's
 }
 
 export interface VoteMessage {
@@ -73,5 +74,53 @@ export const voteActions: { [action_id: string]: VoteMessage } = {
         title: 'I\'ll be right back\nenjoy the blobs',
         choice: [],
         votes: -1
+    },
+    jn_mxm_bp_items: {
+        id: 'jn_mxm_bp_items',
+        title: 'Kies 2 items',
+        choice: [
+            {
+                name: 'Evacuaties Keulen',
+                icon: 'Bomb',
+                id: 'jn_mxm_bp_keulen',
+                // action: ''
+                rundownitems: [
+                    'bp_fullrun_keulen_ident',
+                    'bp_fullrun_keulen_start',
+                    'bp_fullrun_keulen_repo'
+                ]
+            },
+            {
+                name: 'Starttoetsen leraren',
+                icon: 'ClipboardX',
+                id: 'jn_mxm_bp_starttoetsen',
+                rundownitems: [
+                    'bp_fullrun_startexamen_ident',
+                    'bp_fullrun_startexamen_start',
+                    'bp_fullrun_startexamen_repo'
+                ]
+            },
+            {
+                name: 'Opleiding Luchtvaart',
+                icon: 'Plane',
+                id: 'jn_mxm_bp_opleiding_atc',
+                rundownitems: [
+                    'bp_fullrun_atc_ident',
+                    'bp_fullrun_atc_start',
+                    'bp_fullrun_ree_repo'
+                ]
+            },
+            {
+                name: 'Maatregelen maaidood',
+                icon: 'PawPrint',
+                id: 'jn_mxm_bp_maaidood',
+                rundownitems: [
+                    'bp_fullrun_ree_ident',
+                    'bp_fullrun_ree_start',
+                    'bp_fullrun_ree_repo'
+                ]
+            }
+        ],
+        votes: 2
     }
 }
